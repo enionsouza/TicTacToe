ec9e3437dc39428d2180bf9f81258e98b78133af
Chike1990 <chibuikeuzoechina@gmail.com>
2021-04-29 18:08:04 +0100

    Continue user interface interaction features

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,11 @@ require 'colorize'
 
 class Game
   # Class to be implemented inside TicTacToe Module
+  attr_reader :board, :victory
+
   def initialize(game_players)
+    @board = (1..9).to_a
+    @victory = Array.new(9, false)
     puts "TicTacToe Game Class #{game_players}"
   end
 
@@ -23,8 +27,12 @@ end
 
 class Players
   # To be implemented inside TicTacToe Module
+  attr_reader :name1, :name2
+
   def initialize(name1, name2)
-    puts "TicTacToe Players Class #{name1} and #{name2}"
+    @name1 = name1
+    @name2 = name2
+    puts "TicTacToe Players Class #{@name1} and #{@name2}"
   end
 end
 
@@ -32,13 +40,15 @@ def clear_screen
   system('cls') || system('clear')
 end
 
-def print_board
+def print_board(board, victory)
   clear_screen
+  cell = 0
   puts_color(' ' * 15)
   (0..2).each do |i|
     print_color(' ' * 3)
     (0..2).each do |j|
-      print_color(@board[i][j], @victory[i][j])
+      print_color(board[cell], victory[cell])
+      cell += 1
       print_color(' │ ') if j < 2
     end
     puts_color(' ' * 3)
@@ -52,6 +62,7 @@ def puts_color(str)
 end
 
 def print_color(str, green = false)
+  str = str.to_s
   green ? (print str.light_green.on_black) : (print str.light_white.on_black)
 end
 
@@ -70,16 +81,16 @@ game_players = Players.new(name1, name2)
 match = Game.new(game_players)
 player = 0
 loop do
-  print_board
+  print_board(match.board, match.victory)
   print "#{game_players.name1}, please, choose one of the available numbers to play: "
   next unless match.mark?(gets.chomp.to_i)
   if match.winner?
-    print_board
-    puts "#{@players[player][:name]} wins!!!".green.on_black
+    print_board(match.board, match.victory)
+    puts "#{game_players.name1} wins!!!".green.on_black
     break
   end
   if match.cats_game?
-    print_board
+    print_board(match.board, match.victory)
     puts 'Cats Game! No winners for this match!'.red.on_black
     break
   end
