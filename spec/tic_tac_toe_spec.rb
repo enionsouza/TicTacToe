f7098e374c50f1fccf4b2539b1645b7ce05ad966
Enio N. de Souza <enionsouza@gmail.com>
2021-05-04 16:34:38 -0400

    refactor for #check_for_secondary_diagonal?, #winner? and #cats_game? and fix linters

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -4,11 +4,11 @@ RSpec.describe Game do
   describe '#mark?' do
     context 'checks the behavior of #mark?' do
       subject { described_class.new }
-      it 'checking for a valid input' do
+      it 'valid input' do
         expect(subject.mark?(5, 'X')).to eql(true)
       end
-      
-      it 'checking for an invalid input' do
+
+      it 'invalid input' do
         expect(subject.mark?('five'.to_i, 'X')).to eql(false)
       end
     end
@@ -17,12 +17,12 @@ RSpec.describe Game do
   describe '#check_for_rows?' do
     context 'checks the behavior of #check_for_rows?' do
       subject { described_class.new }
-      it 'the first row is a winner' do
+      it 'first row is a winner' do
         subject.board[0] = subject.board[1] = subject.board[2] = 'X'
         expect(subject.check_for_rows?).to eql(true)
       end
 
-      it 'the first row is not a winner' do
+      it 'first row is not a winner' do
         subject.board[0] = 'X'
         subject.board[1] = subject.board[2] = 'O'
         expect(subject.check_for_rows?).to eql(false)
@@ -33,12 +33,12 @@ RSpec.describe Game do
   describe '#check_for_columns?' do
     context 'checks the behavior of #check_for_columns?' do
       subject { described_class.new }
-      it 'if the second column is a winner' do
+      it 'second column is a winner' do
         subject.board[1] = subject.board[4] = subject.board[7] = 'X'
         expect(subject.check_for_columns?).to eql(true)
       end
 
-      it 'if the second column is not a winner' do
+      it 'second column is not a winner' do
         subject.board[1] = 'X'
         subject.board[4] = subject.board[7] = 'O'
         expect(subject.check_for_columns?).to eql(false)
@@ -49,12 +49,12 @@ RSpec.describe Game do
   describe '#check_for_main_diagonal?' do
     context 'checks the behavior of #check_for_main_diagonal?' do
       subject { described_class.new }
-      it 'the main diagonal is a winner' do
+      it 'main diagonal is a winner' do
         subject.board[0] = subject.board[4] = subject.board[8] = 'X'
         expect(subject.check_for_main_diagonal?).to eql(true)
       end
 
-      it 'the main diagonal is not a winner' do
+      it 'main diagonal is not a winner' do
         subject.board[0] = 'X'
         subject.board[4] = subject.board[8] = 'O'
         expect(subject.check_for_main_diagonal?).to eql(false || nil)
@@ -63,46 +63,93 @@ RSpec.describe Game do
   end
 
   describe '#check_for_secondary_diagonal?' do
-    it 'checks if #check_for_secondary_diagonal? is correctly checking if the secondary diagonal is a winner' do
-      game = Game.new
-      game.board[2] = game.board[4] = game.board[6] = 'X'
-      expect(game.check_for_secondary_diagonal?).to eql(true)
-    end
+    context 'checks the behavior of #check_for_secondary_diagonal?' do
+      subject { described_class.new }
+      it 'secondary diagonal is a winner' do
+        subject.board[2] = subject.board[4] = subject.board[6] = 'X'
+        expect(subject.check_for_secondary_diagonal?).to eql(true)
+      end
 
-    it 'checks if #check_for_secondary_diagonal? is correctly checking if the secondary diagonal is a winner' do
-      game = Game.new
-      game.board[2] = 'X'
-      game.board[4] = game.board[6] = 'O'
-      expect(game.check_for_secondary_diagonal?).to eql(false || nil)
+      it 'secondary diagonal is not a winner' do
+        subject.board[2] = 'X'
+        subject.board[4] = subject.board[6] = 'O'
+        expect(subject.check_for_secondary_diagonal?).to eql(false || nil)
+      end
     end
   end
 
   describe '#winner?' do
-    it 'checks if #winner? is correctly checking if one row, or one column or the main diagonal is a winner' do
-      game = Game.new
-      game.board[6] = game.board[7] = game.board[8] = 'X'
-      expect(game.winner?).to eql(true)
+    context 'checks the behavior of #winner? for rows' do
+      subject { described_class.new }
+      it 'third row is a winner' do
+        subject.board[6] = subject.board[7] = subject.board[8] = 'X'
+        expect(subject.winner?).to eql(true)
+      end
+
+      it 'third row is not a winner' do
+        subject.board[6] = 'X'
+        subject.board[7] = subject.board[8] = 'O'
+        expect(subject.winner?).to eql(false || nil)
+      end
     end
 
-    it 'checks if #winner? is correctly checking if the third column is a winner' do
-      game = Game.new
-      game.board[6] = 'X'
-      game.board[7] = game.board[8] = 'O'
-      expect(game.winner?).to eql(false || nil)
+    context 'checks the behavior of #winner? for columns' do
+      subject { described_class.new }
+      it 'third column is a winner' do
+        subject.board[2] = subject.board[5] = subject.board[8] = 'X'
+        expect(subject.winner?).to eql(true)
+      end
+
+      it 'third column is not a winner' do
+        subject.board[2] = 'X'
+        subject.board[5] = subject.board[8] = 'O'
+        expect(subject.winner?).to eql(false || nil)
+      end
+    end
+
+    context 'checks the behavior of #winner? for main diagonal' do
+      subject { described_class.new }
+      it 'main diagonal is a winner' do
+        subject.board[0] = subject.board[4] = subject.board[8] = 'X'
+        expect(subject.winner?).to eql(true)
+      end
+
+      it 'main diagonal is not a winner' do
+        subject.board[0] = 'X'
+        subject.board[4] = subject.board[8] = 'O'
+        expect(subject.winner?).to eql(false || nil)
+      end
+    end
+
+    context 'checks the behavior of #winner? for secondary diagonal' do
+      subject { described_class.new }
+      it 'secondary diagonal is a winner' do
+        subject.board[2] = subject.board[4] = subject.board[6] = 'X'
+        expect(subject.winner?).to eql(true)
+      end
+
+      it 'secondary diagonal is not a winner' do
+        subject.board[2] = 'X'
+        subject.board[4] = subject.board[6] = 'O'
+        expect(subject.winner?).to eql(false || nil)
+      end
     end
   end
 
   describe '#cats_game?' do
-    it 'checks if #cats_game? is correctly evaluating a draw match' do
-      game = Game.new
-      game.board = %w[X O X O O X X X O]
-      expect(game.cats_game?).to eql(true)
-    end
+    context 'checks the behavior of #cats_game?' do
+      subject { described_class.new }
+      it 'draw match' do
+        game = Game.new
+        game.board = %w[X O X O O X X X O]
+        expect(game.cats_game?).to eql(true)
+      end
 
-    it 'checks if #cats_game? is correctly evaluating a draw match' do
-      game = Game.new
-      game.board = [1, 'O', 'X', 'O', 'O', 'X', 'X', 'X', 'O']
-      expect(game.cats_game?).to eql(false)
+      it 'unfinished match' do
+        game = Game.new
+        game.board = [1, 'O', 'X', 'O', 'O', 'X', 'X', 'X', 'O']
+        expect(game.cats_game?).to eql(false)
+      end
     end
   end
 end
